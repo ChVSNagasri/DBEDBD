18159341005e379bd7665b34d3aa1d3436ded89d
ChVSNagasri <2500030484@kluniversity.in>
2026-06-14 10:52:31 +0530

    modified sidebar

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,5 +1,15 @@
-import { Link } from 'react-router-dom'
-import '../styles/sidebar.css'
+import { Link } from 'react-router-dom';
+import '../styles/sidebar.css';
+
+import {
+  FaTachometerAlt,
+  FaBook,
+  FaUsers,
+  FaUserTie,
+  FaList,
+  FaUserCircle,
+  FaSignOutAlt
+} from 'react-icons/fa';
 
 function AdminSidebar() {
   return (
@@ -7,18 +17,40 @@ function AdminSidebar() {
       <h2 className='logo'>📚 Admin</h2>
 
       <nav className='sidebar-nav'>
-        <Link to='/admin/dashboard' className='nav-link'>Dashboard</Link>
-        <Link to='/admin/add-book' className='nav-link'>Add Book</Link>
-        <Link to='/admin/manage-books' className='nav-link'>Manage Books</Link>
-        <Link to='/admin/manage-users' className='nav-link'>Manage Users</Link>
-        <Link to='/admin/add-librarian' className='nav-link'>Add Librarian</Link>
-        <Link to='/admin/categories' className='nav-link'>Categories</Link>
-        <Link to='/admin/profile' className='nav-link'>Profile</Link>
+        <Link to='/admin/dashboard' className='nav-link'>
+          <FaTachometerAlt className='icon' /> Dashboard
+        </Link>
+
+        <Link to='/admin/add-book' className='nav-link'>
+          <FaBook className='icon' /> Add Book
+        </Link>
+
+        <Link to='/admin/manage-books' className='nav-link'>
+          <FaBook className='icon' /> Manage Books
+        </Link>
+
+        <Link to='/admin/manage-users' className='nav-link'>
+          <FaUsers className='icon' /> Manage Users
+        </Link>
+
+        <Link to='/admin/add-librarian' className='nav-link'>
+          <FaUserTie className='icon' /> Add Librarian
+        </Link>
+
+        <Link to='/admin/categories' className='nav-link'>
+          <FaList className='icon' /> Categories
+        </Link>
+
+        <Link to='/admin/profile' className='nav-link'>
+          <FaUserCircle className='icon' /> Profile
+        </Link>
       </nav>
 
-      <Link to='/login' className='logout-link'>Logout</Link>
+      <Link to='/login' className='logout-link'>
+        <FaSignOutAlt className='icon' /> Logout
+      </Link>
     </div>
-  )
+  );
 }
 
-export default AdminSidebar
+export default AdminSidebar;
